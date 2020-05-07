1099f9bdd0007398f355bb49797a5546769cfb6b
betdbc <betcast.25.98@gmail.com>
2020-05-07 00:59:30 -0500

    Se modifico la conexion

--- src/com/example/rest/util/ConectaDB.java
+++ src/com/example/rest/util/ConectaDB.java
@@ -13,7 +13,7 @@ public class ConectaDB {
         public Connection getAcceso() throws Exception {
         	Class.forName("com.mysql.jdbc.Driver");
         	//Connection conn = DriverManager.getConnection("jdbc:mysql://node218902-env-6669914.j.layershift.co.uk/rest","root","MMBeir11727");
-        	Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/proyectocovid","root","mysql");
+        	Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/bd_proyectointegrador","root","mysql");
            	
         return conn;
     }
