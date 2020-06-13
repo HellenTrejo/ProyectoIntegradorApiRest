e3b500e04ce1e54ef0018c12a4d721d4256a8a3e
Hellen <hellentrejo@gmail.com>
2020-06-13 15:16:03 -0500

    Modificando

--- src/com/example/rest/util/ConectaDB.java
+++ src/com/example/rest/util/ConectaDB.java
@@ -12,8 +12,11 @@ public class ConectaDB {
 
         public Connection getAcceso() throws Exception {
         	Class.forName("com.mysql.jdbc.Driver");
+        	
+        	Connection conn = DriverManager.getConnection("jdbc:mysql://node226146-env-6360882.j.layershift.co.uk/proyectocovid","root","ELEmbe26478");
+        	//
         	//Connection conn = DriverManager.getConnection("jdbc:mysql://node225371-env-4252036.j.layershift.co.uk/proyectocovid","root","OQBeie83118");
-        	Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/proyectocovid","root","mysql");
+        	//Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/proyectocovid","root","mysql");
            	
         return conn;
     }
